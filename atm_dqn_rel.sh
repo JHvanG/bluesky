990b70e689f538aa8629d7d584acecb07300959b
JHvanG <jeroen.h.vangelder@gmail.com>
2023-03-30 13:25:02 +0200

    first habrok run results, terminated early

---
+++ atm_dqn_rel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --partition=regular
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=j.h.van.gelder.1@student.rug.nl
+ 
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+
+source /scratch/$USER/.envs/bluesky/bin/activate
+ 
+python3 --version
+which python3
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless
