6704db41f300c4945d3ced0fbc17bda03f1e58b0
Jeroen <jeroen.h.vangelder@gmail.com>
2023-05-25 13:34:31 +0200

    Experiment test

---
+++ atm_sparse3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --time=6-00:00:00
+#SBATCH --mem=2G
+#SBATCH --partition=regular
+#SBATCH --ntasks=6
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=j.h.van.gelder.1@student.rug.nl
+ 
+module purge
+module load Python/3.9.6-GCCcore-11.2.0
+
+source /scratch/$USER/.envs/bluesky/bin/activate
+ 
+python3 --version
+which python3
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=32 --buffer=10000 &
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=32 --buffer=100000 &
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=32 --buffer=1000000 &
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=64 --buffer=10000 &
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=64 --buffer=100000 &
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=SPARSE --batch=64 --buffer=1000000 &
+wait
