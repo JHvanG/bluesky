500e34e338decbbe2ec1a0dde4bf60b9ea285f95
Jeroen <jeroen.h.vangelder@gmail.com>
2023-06-01 09:11:00 +0200

    Actually correct now

--- atm_cpa2comp.sh
+++ atm_cpa2comp.sh
@@ -12,4 +12,4 @@ source /scratch/$USER/.envs/bluesky/bin/activate
  
 python3 --version
 which python3
-python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=2 --reward=SPARSE --batch=32 --buffer=1000000
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=3 --reward=CPA --batch=32 --buffer=100000
