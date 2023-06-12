09c9c0094fba39971b0a9db017ef5a13eecf57ab
Jeroen <jeroen.h.vangelder@gmail.com>
2023-06-12 15:12:11 +0200

    Plots & test job script

--- test.sh
+++ test.sh
@@ -12,4 +12,4 @@ source /scratch/$USER/.envs/bluesky/bin/activate
  
 python3 --version
 which python3
-python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=2 --reward=CPA --batch=32 --buffer=10000
+python3 BlueSky.py --scenfile=dqn_relative.scn --headless --approaches=2 --reward=CPA --batch=128 --buffer=10000
